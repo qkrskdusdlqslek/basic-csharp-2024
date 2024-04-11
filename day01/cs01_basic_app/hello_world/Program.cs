f192e8218d81e8bbb0e49c3c1b41ebe74219f07c
qkrskdusdlqslek <dnftksdurh10@daum.net>
2024-04-11 17:21:10 +0900

    1일차 정리

---
+++ day01/cs01_basic_app/hello_world/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace hello_world // 소스의 가장 큰 단위 namespace == project
+{
+    internal class Program // 접근제한자 class 파일명
+    {
+        static void Main(string[] args) // 정적 void Main ...
+        {
+            // System 네임스페이스 > Console 클래스에 있는 정적메서드 WriteLine()
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
