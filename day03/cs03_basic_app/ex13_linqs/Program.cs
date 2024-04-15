bfe61dd945cde00a433de3eba7f9038e3459363b
qkrskdusdlqslek <dnftksdurh10@daum.net>
2024-04-15 16:40:00 +0900

    3일차 중간정리

---
+++ day03/cs03_basic_app/ex13_linqs/Program.cs
@@ -0,0 +1,150 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ex13_linqs
+{
+    class Profile
+    {
+        private int age;
+        public string Name { get; set; } // 자동 프로퍼티
+        public int Height { get; set; } // 키에 -21억부터 +21억까지 들어감
+        public int Age {
+            get => age;
+            set 
+                {
+                    if (value >= 0 && value < 200) { age = value; }
+                    else { age = 20; } // 잘못 넣었으면 20으로 fix
+                }
+            }
+        }
+
+    class Product
+    {
+        public string Title { get; set; }
+        public string Star { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ 미사용 테스트!");
+
+            Profile[] arrprofiles =
+            {
+                new Profile { Name = "정우성", Height = 186, Age = 49 },
+                new Profile { Name = "이정재", Height = 184, Age = 49 },
+                new Profile { Name = "김태희", Height = 158, Age = 46 },
+                new Profile { Name = "전지현", Height = 172, Age = 44 },
+                new Profile { Name = "이문세", Height = 180, Age = 55 },
+                new Profile { Name = "장원영", Height = 165, Age = 24 },
+                new Profile { Name = "RM", Height = 175, Age = 29 }
+            };
+
+            Product[] arrproducts =
+            {
+                new Product { Title = "비트", Star = "정우성" },
+                new Product { Title = "오징어게임", Star = "이정재" },
+                new Product { Title = "엽기적인 그녀", Star = "전지현" },
+                new Product { Title = "도둑들", Star = "전지현" },
+                new Product { Title = "Dynamite", Star = "RM" },
+                new Product { Title = "Solo 예찬", Star = "이문세" }
+            };
+
+            // LINQ 미사용
+            List<Profile> profiles = new List<Profile>();
+            foreach (Profile profile in arrprofiles)
+            {
+                if (profile.Height < 175)
+                    profiles.Add(profile);
+            }
+
+            profiles.Sort(
+                (profile1, profile2) =>
+                {
+                    return profile1.Height - profile2.Height;
+                });
+
+            foreach (var profile in profiles)
+            {
+                Console.WriteLine($"{profile.Name}, ({profile.Age}세), {profile.Height}cm)");
+            } // LINQ를 사용하지 않으면 여기까지 15줄 코딩
+
+            // LINQ 사용하면
+            Console.WriteLine("LINQ 사용 테스트!");
+            var profiles2 = from profile in arrprofiles
+                            where profile.Height < 175
+                            orderby profile.Height
+                            select profile;
+
+            foreach (var profile in profiles2)
+            {
+                Console.WriteLine($"{profile.Name}, ({profile.Age}세), {profile.Height}cm)");
+            } // LINQ를 사용하면 8줄 코딩
+
+            // LINQ 기본
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var result = from n in numbers
+                         where n % 2 == 0
+                         orderby n descending // 내림차순으로 해서 출력
+                         select n;
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            // group by - DB의 group by처럼 집계함수가 필요X
+            var groupProfiles = from p in arrprofiles
+                                orderby p.Height descending // 키가 큰 순으로 출력
+                                group p by p.Height < 175 into g
+                                select new { GroupKey = g.Key, Profiles = g };
+
+            foreach (var group in groupProfiles)
+            {
+                Console.WriteLine($"- 175cm 미만? : {group.GroupKey}");
+
+                foreach (var profile in group.Profiles)
+                {
+                    Console.WriteLine($">>> ({profile.Name}), ({profile.Age}세), ({profile.Height}cm)");
+                }
+            }
+
+            // LINQ JOIN
+            var innerJoinResult = from pf in arrprofiles
+                                  join pr in arrproducts
+                                  on pf.Name equals pr.Star
+                                  select new
+                                  {
+                                      Name = pf.Name,
+                                      Work = pr.Title,
+                                      Height = pf.Height,
+                                      Age = pf.Age
+                                  };
+
+            Console.WriteLine("내부조인 결과");
+            foreach (var item in innerJoinResult)
+            {
+                Console.WriteLine($"작품 : {item.Work} / 이름 : {item.Name} / 나이 : {item.Age}");
+            }
+
+            var outerJoinResult = from pf in arrprofiles
+                                  join pr in arrproducts
+                                  on pf.Name equals pr.Star 
+                                  into ps // 외부조인시 내부조인 LINQ에 추가되는 부분
+                                  from pr in ps.DefaultIfEmpty(new Product() { Title = "작품없음" }) // 내부조인 LINQ에 추가되는 부분
+                                  select new
+                                  {
+                                      Name = pf.Name,
+                                      Work = pr.Title,
+                                      Height = pf.Height,
+                                      Age = pf.Age
+                                  };
+            Console.WriteLine("외부조인 결과!");
+            foreach (var item in outerJoinResult)
+            {
+                Console.WriteLine($"작품 : {item.Work} / 이름 : {item.Name} / 나이 : {item.Age}");
+            }
+
+        }
+    }
+}
---
+++ day03/cs03_basic_app/ex14_attributes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Reflection;
+
+namespace ex14_attributes
+{
+    class MyClass
+    {
+        [Obsolete("이 메소드는 다음 버전에서 폐기됩니다. NewMethod()를 사용하세요.")] // ,true는 아예 사용불가(false는 사용가능)
+        /// <summary>
+        /// 올드메소드, 이렇게 쓰셈
+        /// </summary>
+        public void OldMethod() // 최초 제작 메소드
+        {
+            Console.WriteLine("Old Method!");
+        }
+
+        /// <summary>
+        /// 뉴메소드, 제발 이걸 쓰셈
+        /// </summary>
+        public void NewMethod() // 개선한 매소드
+        {
+            Console.WriteLine("New Method!");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region '리플렉션'
+            Console.WriteLine("리플렉션"); // GetType();만 알고 있으면 됨
+
+            int a = int.MaxValue;
+            Type type = a.GetType();
+            Console.WriteLine(type.FullName); // System.Int32
+
+            float f = float.MaxValue;
+            Console.WriteLine(f.GetType()); // System.Single
+
+            double d = double.MaxValue;
+            Console.WriteLine(d.GetType()); // System.Double
+
+            // Advanced 개발시에 필요한 내용
+            FieldInfo[] fields = type.GetFields(); // 타입 객체에서 어떤 필드가 있는지 모두 확인
+            foreach (var item in fields)
+            {
+                Console.WriteLine($"Type : {item.FieldType}, Name : {item.Name}");
+            }
+
+            MethodInfo[] methods = type.GetMethods(); // 타입 객체에서 어떤 메소드가 있는지 모두 확인
+            foreach (var item in methods)
+            {
+                Console.WriteLine($"Type : {item.DeclaringType}, Name : {item.Name}");
+            }
+            #endregion
+
+            // 애트리뷰트
+            Console.WriteLine("애트리뷰트!");
+
+            MyClass myclass = new MyClass();
+            // myclass.OldMethod();
+            myclass.NewMethod();
+        }
+    }
+}
